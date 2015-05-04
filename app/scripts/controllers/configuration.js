9300e263e1c628e49a4b0e9e27b953c1fa1235e8
Thiago Verney <thiago.verney@gmail.com>
2015-05-04 15:13:59 -0300

    Commit ordenação passos

--- app/scripts/controllers/configuration.js
+++ app/scripts/controllers/configuration.js
@@ -7,45 +7,66 @@
  * # AboutCtrl
  * Controller of the exemplosApp
  */
-app.controller('ConfigurationCtrl', function ($scope) {
+app.controller('ConfigurationCtrl', function($scope) {
 
-  	var cabecalho = [
-    	{ 'name' : '1 Coluna', 'column' : 1 },    	
-    	{ 'name' : '2 Colunas', 'column' : 2 },    	
-    	{ 'name' : '3 Colunas', 'column' : 3 }
-    ];
+    var cabecalho = [{
+        'name': '1 Coluna',
+        'column': 1
+    }, {
+        'name': '2 Colunas',
+        'column': 2
+    }, {
+        'name': '3 Colunas',
+        'column': 3
+    }];
 
-    var detalhe = [
-    	{ 'name' : '1 Coluna', 'column' : 1 }
-    ];
+    var detalhe = [{
+        'name': '1 Coluna',
+        'column': 1
+    }];
 
-    var rodape = [
-    	{ 'name' : '1 Coluna', 'column' : 1, 'size': [100] },    	
-    	{ 'name' : '2 Colunas', 'column' : 2, 'size': [40,60] },    	
-    	{ 'name' : '3 Colunas', 'column' : 3, 'size': [50,20,30] }
-    ];
+    var rodape = [{
+        'name': '1 Coluna',
+        'column': 1,
+        'size': [100]
+    }, {
+        'name': '2 Colunas',
+        'column': 2,
+        'size': [40, 60]
+    }, {
+        'name': '3 Colunas',
+        'column': 3,
+        'size': [50, 20, 30]
+    }];
 
     $scope.option = {
-    	'cabecalho': cabecalho,
-    	'detalhe': detalhe,
-    	'rodape': rodape
+        'cabecalho': cabecalho,
+        'detalhe': detalhe,
+        'rodape': rodape
     };
 
     $scope.option.selection = {
-    	'cabecalho': cabecalho[0],
-    	'detalhe': detalhe[0],
-    	'rodape': rodape[0]
+        'cabecalho': cabecalho[0],
+        'detalhe': detalhe[0],
+        'rodape': rodape[0]
     };
 
     //define os menus superiores para o wizard
     $scope.configuracao = {};
-    $scope.configuracao.menus = [{ordem:1,
-        titulo:"Layout"
-    }, {ordem:2, titulo:"Componentes"},{ordem:3,
-        titulo:"Configuração"
-    }
-    ];
+    $scope.configuracao.menus = [{
+        ordem: 1,
+        titulo: "Visão"
+    },{
+        ordem: 2,
+        titulo: "Layout"
+    }, {
+        ordem: 3,
+        titulo: "Campos"
+    }, {
+        ordem: 4,
+        titulo: "Configuração"
+    }];
 
-    
 
-  });
+
+});
--- app/scripts/directives/wizard/wizard.js
+++ app/scripts/directives/wizard/wizard.js
@@ -4,7 +4,30 @@ app.directive("wizard", function() {
         restrict: "E",
         transclude:true,
         scope: {
-        	configuracao:"="
+        	configuracao:"=",
+        	steps:"="
+        },
+        link: function(scope){
+        	scope.atual = 1;
+
+        	scope.proximo = function(){
+        		if(scope.atual < scope.steps){
+        			scope.atual = scope.atual + 1;
+        		}
+        	};
+
+        	scope.anterior = function(){
+				if(scope.atual <= scope.steps){
+        			scope.atual = scope.atual - 1;
+        		}
+        	};
+
+        	scope.isAtual = function(order){
+        		if(order === scope.atual){
+        			return true;
+        		}
+        	}
+
         }
     }
 });
