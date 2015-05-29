550a0526e92017e193cc480df9ecf7d5603467ef
rafael.schlemper <rafael.schlemper@gennera.com.br>
2015-05-29 17:49:27 -0300

    liks das páginas

--- app/scripts/controllers/report.js
+++ app/scripts/controllers/report.js
@@ -26,8 +26,8 @@ app.controller('ReportCtrl', function($scope, $filter, $routeParams, $location,
     }
 
     var getVisio = function() {
-        VisioService.service.getByHashid($routeParams.hashid)
-        // JsonService.visioTest()
+        // VisioService.service.getByHashid($routeParams.hashid)
+        JsonService.visioTest()
             .then(function(data) {
                 visio = data[0];                
                 $scope.visio = angular.copy(visio);
@@ -42,7 +42,6 @@ app.controller('ReportCtrl', function($scope, $filter, $routeParams, $location,
     $scope.getPage = function(page) {               
         $scope.visio = angular.copy(visio);
         ReportService.page(page, registers, $scope.visio.layout);
-        console.log($scope.pages);
     }
 
     createReport();
